9f1668e02a1308f2aa6035abd25044dfc7656cce
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2022-10-28 14:41:02 +0200

    deleted one print related to debugging

--- zoya/zoya_site/views.py
+++ zoya/zoya_site/views.py
@@ -68,7 +68,6 @@ class UpdateLvl(APIView):
             serializer.save()
             return Response(None, status=status.HTTP_201_CREATED)
 
-        print(serializer.errors)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
